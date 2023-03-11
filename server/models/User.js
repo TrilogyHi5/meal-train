a190e19f8b143a6ae9e8c0fac490440265a6ec5c
flgaitan <fiama_gaitan92@hotmail.com>
2023-03-11 15:52:48 -0600

    added user credentials for  USer model

--- server/models/User.js
+++ server/models/User.js
@@ -5,6 +5,26 @@ const bcrypt = require('bcrypt');
 //const  = require('./');
 
 const userSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+      lastName: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+      },
+      password: {
+        type: String,
+        required: true,
+        minlength: 5,
+      },
     height: {
         type: Number,
         required: true
