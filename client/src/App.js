bb78b610f548a8df26335447a20c9765a0dd95d4
mmm <margaret.m.matson@gmail.com>
2023-03-12 20:34:45 -0500

    added pages and utils for client

--- client/src/App.js
+++ client/src/App.js
@@ -1,61 +1,27 @@
 //import logo from './logo.svg';
+import React from 'react';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+// import Header from './components/Header';
+// import Footer from './components/Footer';
+import Ingredients from './pages/Ingredients'
 import './App.css';
-import IndexPage from './utils'
 
 function App() {
   return (
-    <IndexPage />
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+    <Login />,
+    <Signup />,
+    <Ingredients />
   );
 }
-
-function Form() {
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState(''); // Create a helper for this one?
-  const [age, setAge] = useState('');
-  return (
-    <div>
-      <label>
-        First Name:
-        <input value={firstName} onChange={e => setFirstName(e.target.value)} />
-      </label>
-      <label>
-        Last Name:
-      <input value={lastName} onChange={e => setLastName(e.target.value)} />
-      </label>
-      <label>
-        Email:
-        <input value={email} onChange={e => setEmail(e.target.value)} />
-      </label>
-      <label>
-        Password:
-        <input value={password} onChange={e => setPassword(e.target.value)} />
-      </label>
-      <label>
-        Age:
-        <input value={age} onChange={e => setAge(e.target.value)} />
-      </label>
-    </div>
-  )
-}
-
-Form();
 
 export default App;
