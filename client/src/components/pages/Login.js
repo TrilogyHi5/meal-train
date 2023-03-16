f34be544accaa7cd7c2cc567c5d6ac7418dcae75
mmm <margaret.m.matson@gmail.com>
2023-03-15 20:33:21 -0500

    set up page navigation

--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -1,25 +1,22 @@
 import React, { useState } from 'react';
 // import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
-import { ADD_USER } from '../utils/mutations';
-// import Auth from '../utils/auth';
-// import { useState } from 'react'
+import { LOGIN_USER } from '../../utils/mutations';
+//import Auth from '../utils/auth';
+
 import { ChevronDownIcon } from '@heroicons/react/20/solid'
 import { Switch } from '@headlessui/react'
 
-//tailwind css code
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
-const Signup = () => {
+const Login = (props) => {
   const [formState, setFormState] = useState({
-    firstName: '',
-    lastName: '',
     email: '',
     password: '',
   });
 
-  const [addUser, { data, error }] = useMutation(ADD_USER);
+  const [login, { data, error }] = useMutation(LOGIN_USER);
 
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -29,25 +26,38 @@ const Signup = () => {
       [name]: value,
     });
   };
-
+  // submit form
   const handleFormSubmit = async (event) => {
     event.preventDefault();
     console.log(formState);
 
     try {
-      const { data } = await addUser({
+      const { data } = await login({
         variables: { ...formState },
       });
 
-      // Auth.login(data.addUser.token);
-    } catch (e) {
+      //   Auth.login(data.login.token);
+      // } catch (e) {
+      //   console.error(e);
+      // }
+
+    }
+    catch (e) {
       console.error(e);
     }
+    // clear form values
+    setFormState({
+      email: '',
+      password: '',
+
+    });
+
   };
 
+  // export default function Login() {
   return (
     <div className="isolate bg-white py-24 px-6 sm:py-32 lg:px-8">
-      <div className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]">
+      {/* <div className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]">
         <svg
           className="relative left-1/2 -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-40rem)] sm:h-[42.375rem]"
           viewBox="0 0 1155 678"
@@ -71,53 +81,21 @@ const Signup = () => {
             </linearGradient>
           </defs>
         </svg>
-      </div>
+      </div> */}
       <div className="mx-auto max-w-2xl text-center">
-        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Sign Up</h2>
+        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Login</h2>
         <p className="mt-2 text-lg leading-8 text-gray-600">
-          Tell us more about yourself.
+          Welcome back!
         </p>
       </div>
-      <div>{data ? (
+      {data ? (
         <p>
-          Success! Next head to {' '}
-          <a to="/">to personalize your meal plan.</a>
+          Success! You may now head{' '}
+          <a h="/">back to the homepage.</a>
         </p>
       ) : (
         <form onSubmit={handleFormSubmit} action="#" method="POST" className="mx-auto mt-16 max-w-xl sm:mt-20">
           <div className="grid grid-cols-1 gap-y-6 gap-x-8 sm:grid-cols-2">
-            <div>
-              <label htmlFor="first-name" className="block text-sm font-semibold leading-6 text-gray-900">
-                First name
-              </label>
-              <div className="mt-2.5">
-                <input
-                  type="text"
-                  name="first-name"
-                  id="first-name"
-                  autoComplete="given-name"
-                  defaultValue={formState.firstName}
-                  className="form-input block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  onChange={handleChange}
-                />
-              </div>
-            </div>
-            <div>
-              <label htmlFor="last-name" className="block text-sm font-semibold leading-6 text-gray-900">
-                Last name
-              </label>
-              <div className="mt-2.5">
-                <input
-                  type="text"
-                  name="last-name"
-                  id="last-name"
-                  autoComplete="family-name"
-                  defaultValue={formState.lastName}
-                  className="form-input block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  onChange={handleChange}
-                />
-              </div>
-            </div>
             <div className="sm:col-span-2">
               <label htmlFor="email" className="block text-sm font-semibold leading-6 text-gray-900">
                 Email
@@ -128,8 +106,8 @@ const Signup = () => {
                   name="email"
                   id="email"
                   autoComplete="email"
-                  defaultValue={formState.email}
-                  className="form-input block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  value={formState.email}
+                  className="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   onChange={handleChange}
                 />
               </div>
@@ -144,8 +122,8 @@ const Signup = () => {
                   name="password"
                   id="password"
                   autoComplete="password"
-                  defaultValue={formState.password}
-                  className="form-input block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  value={formState.password}
+                  className="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   onChange={handleChange}
                 />
               </div>
@@ -156,18 +134,19 @@ const Signup = () => {
               type="submit"
               className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
-              Get Started
+              Login
             </button>
           </div>
         </form>
       )}
-        {error && (
-          <div className="my-3 p-3 bg-danger text-white">
-            {error.message}
-          </div>
-        )}
-      </div>
+      {error && (
+        <div className="my-3 p-3 bg-danger text-white">
+          {error.message}
+        </div>
+      )}
     </div>
-  )
-}
-export default Signup;
+  );
+
+};
+
+export default Login;
