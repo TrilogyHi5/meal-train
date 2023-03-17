83f3bd5f2db55de493cebe35a9de8e3b82990420
Andy Phinney <111609415+aphinney0516@users.noreply.github.com>
2023-03-16 22:49:34 -0400

    Added content to homnepage, and added photo section to about page

--- client/src/components/pages/HomePage.js
+++ client/src/components/pages/HomePage.js
@@ -16,8 +16,11 @@ function HomePage() {
       {/* Main content */}
       <div className="flex-1 flex items-center justify-center ">
         <div className="text-center ">
-          <h1 className="text-4xl font-bold mb-4">Welcome to Meal Train</h1>
-          <p className="text-lg mb-8">Text about Meal Train Text about Meal Train Text about Meal Train Text about Meal Train Text about Meal Train </p>
+          <h1 className="text-4xl font-bold mb-4">Meal Train - Train yourself to diet differently</h1>
+          <p className="text-lg mb-8">Are you frustrated with fad diets and ready to try something different? 
+              Then it is time to train yourself to diet differently, with Meal Train!
+              Meal Train takes all of the leg work out of dieting and gives you complete daily meal plans 
+              that are easy to cook made from healthy common grocery store ingredients. </p>
         </div>
       </div>
       <div className="flex justify-center items-center">
