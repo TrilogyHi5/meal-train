b9d948620aae86078ccb00d35e1654b4ebb85f57
Jeff <jcgilbert70@gmail.com>
2023-03-19 17:11:00 -0500

    fixed header, footer, and the tab name

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -4,15 +4,15 @@ import icon from '../../utils/images/icon.png' // import icon image
 export default function Footer() {
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
-        return (
-            <footer className="bg-gray-900 text-white py-4">
-                <div className="container mx-auto text-center">
-                    <img src={icon} className="w-9 h-9"></img>
-                    <div className="flex-1">
-                        <p className="flex items-center h-full text-white font-display text-4xl text-center pl-4" >Meal Train</p>
-                    </div>
-                    <p>Copyright © 2023</p>
+    return (
+        <footer className="bg-gray-900 text-white py-4">
+            <div className="container mx-auto text-center flex items-center">
+                <img src={icon} className="w-9 h-9"></img>
+                <div className="flex-1">
+                    <p className="flex items-center h-full text-white font-display text-4xl text-center pl-4" >Meal Train</p>
                 </div>
-            </footer>
-        );
-    }
+                <p>Copyright © 2023</p>
+            </div>
+        </footer>
+    );
+}
